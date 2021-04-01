4be13d2f5b56ef2806320a8307d3cbd43954d200
Lodewikes <greyvenstein.louw@gmail.com>
2021-04-01 11:15:26 +0000

    handle same start and end year; style fixes

--- lab1/population/population.c
+++ lab1/population/population.c
@@ -6,44 +6,53 @@ int calculate(int startPop, int endPop);
 
 int main(void)
 {
-	int startPop = 0;
-	int endPop = 0;
-	int years;
+    int startPop = 0;
+    int endPop = 0;
+    int years;
 
-	// promt user for start population size
-	do
-	{
-		startPop = get_int("Start size: ");
-	}
-	while(startPop < 9);
+    // promt user for start population size
+    do
+    {
+        startPop = get_int("Start size: ");
+    }
+    while (startPop < 9);
 
-	// prompt end population size
-	do
-	{
-		endPop = get_int("End size: ");
-	}
-	while(endPop < startPop);
+    // prompt end population size
+    do
+    {
+        endPop = get_int("End size: ");
+    }
+    while (endPop < startPop);
 
-	// Calculate int nr years required for pop to reach at least end size
-	years = calculate(startPop, endPop);
-	// print nr years required 
-	printf("Years: %i\n", years);
+    // 0 years if start and end is same
+    if (startPop == endPop)
+    {
+        printf("Years: 0\n");
+    }
+    else
+    {
+        // Calculate int nr years required for pop to reach at least end size
+        years = calculate(startPop, endPop);
+        // print nr years required
+        printf("Years: %i\n", years);
+    }
 }
 
-int calculate(int startPop, int endPop) 
+// function to calculate year
+int calculate(int startPop, int endPop)
 {
-	int newPop = startPop;
-	int deaths = 0;
-	int births = 0;
-	int years = 0;
+    int newPop = startPop;
+    int deaths = 0;
+    int births = 0;
+    int years = 0;
 
-	do 
-	{
-		deaths = newPop / 4;
-		births = newPop / 3;
-		newPop = newPop + births - deaths;
-		years++;
-	}	
-	while(newPop < endPop);
-	return years;
+    do
+    {
+        deaths = newPop / 4;
+        births = newPop / 3;
+        newPop = newPop + births - deaths;
+        years++;
+    }
+    while (newPop < endPop);
+    return years;
 }

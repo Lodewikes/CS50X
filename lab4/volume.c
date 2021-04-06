48d235c2e2fffe58c56371598dc107305a86829e
Lodewikes <greyvenstein.louw@gmail.com>
2021-04-06 08:02:27 +0200

    implemented lab4/

--- lab4/volume.c
+++ lab4/volume.c
@@ -34,8 +34,18 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
+	uint8_t header[HEADER_SIZE];
+	fread(header, HEADER_SIZE, 1, input);
+	fwrite(header, HEADER_SIZE, 1, output);
 
     // TODO: Read samples from input file and write updated data to output file
+	int16_t buffer;
+	do 
+	{
+		buffer = buffer * factor;
+		fwrite(&buffer, sizeof(int16_t), 1, output);
+	}
+	while (fread(&buffer, sizeof(int16_t), 1, input));
 
     // Close files
     fclose(input);
